5e98173f946342ffbedc9e22aa7b19394fa7aa7f
s w <w@ss-MacBook-Air.local>
2023-12-25 04:00:58 -0700

    first commit

---
+++ api/task.js
@@ -0,0 +1,80 @@
+import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { 
+    UpdateCommand, 
+    PutCommand, 
+    DynamoDBDocumentClient, 
+    ScanCommand, 
+    DeleteCommand 
+} from "@aws-sdk/lib-dynamodb";
+import crypto from "crypto"
+
+const client = new DynamoDBClient({ region: "us-east-1"});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const fetchTasks = async () => {
+    //performs fetch all so change when you mod system
+    const command = new ScanCommand({
+        ExpressionAttributeNames: {"#name": "name" },
+        ProjectionExpression: "id, #name, completed",
+        TableName: "Tasks",
+    });
+
+    const response = await docClient.send(command);
+
+    return response;
+};
+
+export const createTasks = async (name, completed) => {
+    const uuid = crypto.randomUUID()
+    //
+    const command = new PutCommand({
+        TableName: "Tasks",
+        Item: {
+            id: "",
+            name,
+            completed
+        }
+    })
+    
+    const response = await docClient.send(command);
+
+    return response;
+};
+
+export const updateTasks = async (id, name, completed) => {
+    //performs fetch all so change when you mod system
+    const command = new UpdateCommand({
+        TableName: "Tasks",
+        Key: {
+            id
+        },
+        ExpressionAttributeNames: {
+            "#name": "name"
+        },
+        UpdateExpression: "set #name = :n, completed = :c",
+        ExpressionAttributeValues: {
+            ":n": name,
+            ":c": completed
+        },
+        ReturnValues: "ALL_NEW"
+    })
+    
+    const response = await docClient.send(command);
+
+    return response;
+};
+
+export const deleteTasks = async (id) => {
+    //performs fetch all so change when you mod system
+    const command = new DeleteCommand({
+        TableName: "Tasks",
+        Key: {
+            id
+        }
+    })
+    
+    const response = await docClient.send(command);
+
+    return response;
+};
+
---
+++ ui/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { AddTaskForm } from './components/AddTaskForm';
+import { Task } from './components/Task';
+import axios from "axios";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+
+
+export default function App() {
+  const[task, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    try {
+      const { data } = await axios.get(API_URL);
+
+      setTasks(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <AddTaskForm fetchTasks={fetchTasks} />
+      {tasks.map((task) => (
+        <Task task={task} key={task.id} fetchTasks={fetchTasks} /> 
+      ))}
+      
+    </ThemeProvider>
+  );
+}
